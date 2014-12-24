e7225c1621619bcfab180719b1c3d2c558e5be02
Devin Rhode <devinrhode2@gmail.com>
2014-12-23 21:11:28 -0800

    bower install ember-pouch --save

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -12,6 +12,10 @@ var app = new EmberApp({
   }
 });
 
+app.import('bower_components/pouchdb/dist/pouchdb.js');
+app.import('bower_components/relational-pouch/dist/pouchdb.relational-pouch.js');
+app.import('bower_components/ember-pouch/dist/globals/main.js');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
